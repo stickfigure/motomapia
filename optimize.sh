3f464b340f8ca4bdd16923df430b697eb802a4f8
Jeff Schnitzer <jeff@infohazard.org>
2012-07-05 23:26:00 -0700

    FINALLY got the paths to work for the optimizer

---
+++ optimize.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# optimize one file in the war/js/app/page dir
+# r.js is too damn sensitive to where it gets executed
+# $1 should be a pathless file in the page dir
+# shoudl be executed from inside the war dir
+
+r.js -o name=app/page/`basename $1 .js` out=../target/staging/js/app/page/$1 baseUrl=js
